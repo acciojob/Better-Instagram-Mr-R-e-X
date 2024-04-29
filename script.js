ce8e745de9460bf567f86831a7f8041b0fc85771
Souvik Hazra <117592326+Mr-R-e-X@users.noreply.github.com>
2024-04-29 13:09:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 //your code here
+let items = document.querySelectorAll(".image");
+let draggedItem = null;
+items.forEach((item) => {
+  item.addEventListener("dragstart", dragStart);
+  item.addEventListener("dragover", dragOver);
+  item.addEventListener("drop", dragDrop);
+});
+function dragStart(e) {
+  draggedItem = this;
+}
+function dragOver(e) {
+  e.preventDefault();
+}
+
+function dragDrop(e) {
+  let parentItem = this.parentNode;
+  const temp = document.createElement("div");
+  parentItem.insertBefore(temp, draggedItem);
+  parentItem.insertBefore(draggedItem, this);
+  parentItem.insertBefore(this, temp);
+  parentItem.removeChild(temp);
+}
